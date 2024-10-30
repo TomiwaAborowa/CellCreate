2f495854554b951f6de5b445bd873e5e6245d5a4
Tomiwa Aborowa <tomiwaw2013@gmail.com>
2024-10-30 16:20:26 +0100

    some text

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -8,20 +8,29 @@ function Home() {
 <div className="bg-[url('bgbrown.jpg')] bg-contain">      
    <div className="bg-contain bg-right mix-blend bg-no-repeat h-screen w-screen bg-[url('pic1.png')]">
 
-  <nav className="flex justify-center items-center">
-  <img src="fresh.png" alt="logo" className="p-5 h-24 w-24" />
-  </nav>
-  <div className="text-center p-24 ">
-    <p className="text-white font-medium tracking-wider text-2xl ml-2 sm:text-xl">
+  <div className="text-center p-10 ">
+  <h1 className="text-4xl md:text-5xl font-extrabold text-white text-center my-8">
+  <span className="block text-transparent bg-clip-text bg-gradient-to-r from-slate-900 to-neutral-800">
+    Welcome to
+  </span>
+  <span className="block bg-clip-text text-transparent bg-gradient-to-r from-amber-50 to-gray-50">
+    ANORA CELL GROUP
+  </span>
+</h1>
+<p className="mt-4 text-2xl md:text-xl text-amber-200 font-bold">
+    Join a cell today !!!
+  </p>
+   
+    {/* <p className="text-white font-medium tracking-wider text-2xl ml-2 sm:text-xl">
       Welcome to
     </p>
     <h2 className="text-3xl font-bold mt-5 tracking-wide text-white sm:text-6xl">
       ANORA CELL
       <br />
       GROUP
-    </h2>
+    </h2> */}
   </div>
-      <div className="flex items-center justify-center gap-x-6 lg:justify-start">
+      <div className="flex mt-64 items-center justify-center gap-x-6 lg:justify-start ">
         <button
           type="button"
           className="inline-flex items-center rounded-md sm:mx-auto hover:bg-amber-800 bg- px-3 py-2 font-bold text-gray-50 shadow-sm ring-1 ring-inset ring-gray-300"
